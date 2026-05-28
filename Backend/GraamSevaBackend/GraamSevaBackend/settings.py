e215d8b2fb40863f364d583c7f508e3a824eb4b7
Bhavy <bhavy_mr@cs.iitr.ac.in>
2026-05-28 15:32:36 +0530

    Remove tracked env file and require SECRET_KEY from environment

--- Backend/GraamSevaBackend/GraamSevaBackend/settings.py
+++ Backend/GraamSevaBackend/GraamSevaBackend/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fm^%z6r3=!dma0ly7tz%viio&2pf2(kaf@uq!o3*1&glk1gj!1'
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ImproperlyConfigured('Missing required environment variable: SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
